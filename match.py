1104dc79bc4bd7ebc7cbb82e994d1450a8874968
JakeSmith01 <jake.smith@my.jcu.edu.au>
2023-10-22 20:46:57 +1000

    Refactor scoreGenerator function to only have one return, add comments explaining code

--- match.py
+++ match.py
@@ -3,11 +3,13 @@ import random
 def scoreGenerator():
     homeScore = tryGenerator()
     awayScore = tryGenerator()
-
+    extraTime = False
+    didDraw = False
+    
     # If scores are level decide if games end in a draw or is won in extra time
     if homeScore == awayScore:
         drew = random.choices([True, False], weights=(5,95))
-
+        extraTime = True
         # If teams dont draw, decide winning method and winning team
         if drew[0] == False:
             winMethod = random.choices(["fg", "try"], weights=(80,20))
@@ -21,16 +23,17 @@ def scoreGenerator():
                 if winner == "home":
                     homeScore += 4
                 else:
-                    awayScore +=4
-            return([homeScore, awayScore, True, False])                          #TODO: Fix this return so its just one return. 
-        else:                                                                     # Also make it more clear that the third value is if they go to extra time and the last value is if they drew
-            return([homeScore, awayScore, True, True])
+                    awayScore +=4                   
+        else:                                                                   
+            didDraw = True
     else:
-        return([homeScore, awayScore, False, False])
+        return([homeScore, awayScore, extraTime, didDraw])
 
 def tryGenerator():
+    # Randomly pick if a team scores or not with the team scoring being 80% likely.
     tryScored = random.choices([True, False], weights=(80,20))
     if tryScored[0]:
+        # Randomly pick category and then randomly generate number of tries based on category
         amountScored = random.choices(["small", "moderate", "large", "massive","supermassive"], weights=(35,50,10,4,1))
         if amountScored[0] == "small":
             tries = random.randint(1,2)
@@ -42,6 +45,7 @@ def tryGenerator():
             tries = random.randint(8,10)
         elif amountScored[0] == "supermassive":
             tries = random.randint(11,16)
+        # Randomly determine how many tries are converted.
         convertedTries = random.randint(0,tries)
         return((tries * 4) + (convertedTries * 2))
     else:
