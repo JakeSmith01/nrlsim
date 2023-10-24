dd5068b39b2ad3eaeb27055dd57d248ac8a0c809
JakeSmith01 <jake.smith@my.jcu.edu.au>
2023-10-24 19:48:38 +1000

    Create format.py

---
+++ format.py
@@ -0,0 +1,5 @@
+# Solution found from following stackoverflow article https://stackoverflow.com/questions/55065640/python-string-formatter-to-get-underlined-headline
+
+class Format:
+    end = '\033[0m'
+    underline = '\033[4m'
